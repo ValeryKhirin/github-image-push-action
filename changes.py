50d6298ed739b4f696b649449d52a075a3f6f4cc
Valery Khirin <valerykhirin@dalpakab-pc.rb4u.co.il>
2023-02-14 15:38:43 +0200

    debug 18 - new with changes.py -on script

--- changes.py
+++ changes.py
@@ -31,23 +31,27 @@ for item in workflows:
 
 # Print a message indicating whether or not changes were detected, and show what changed
 if folder_changed:
+    # print("Changes were detected in the .github/workflows folder:")
+    # for file_path in changed_files:
+    #     file = repo.get_contents(file_path)
+    #     previous_commit = repo.get_commits(path=file_path, sha=default_branch)[0]
+    #     diff = repo.compare(previous_commit.sha, file.sha).diff_url
+    #     print(f"\nChanges in {file_path}: {diff}\n")
+
+    #     # Parse the YAML content
+    #     content = file.decoded_content.decode("utf-8")
+    #     data = yaml.safe_load(content)
+
+    #     # Print the changes in the YAML content
+    #     previous_content = repo.get_contents(path=file_path, ref=previous_commit.sha).decoded_content.decode("utf-8")
+    #     previous_data = yaml.safe_load(previous_content)
+    #     print("Changes in the YAML content:")
+    #     for key in data.keys():
+    #         if key not in previous_data.keys() or data[key] != previous_data[key]:
+    #             print(f"{key}: {previous_data.get(key, '')} -> {data[key]}")
     print("Changes were detected in the .github/workflows folder:")
-    for file_path in changed_files:
-        file = repo.get_contents(file_path)
-        previous_commit = repo.get_commits(path=file_path, sha=default_branch)[0]
-        diff = repo.compare(previous_commit.sha, file.sha).diff_url
-        print(f"\nChanges in {file_path}: {diff}\n")
-
-        # Parse the YAML content
-        content = file.decoded_content.decode("utf-8")
-        data = yaml.safe_load(content)
-
-        # Print the changes in the YAML content
-        previous_content = repo.get_contents(path=file_path, ref=previous_commit.sha).decoded_content.decode("utf-8")
-        previous_data = yaml.safe_load(previous_content)
-        print("Changes in the YAML content:")
-        for key in data.keys():
-            if key not in previous_data.keys() or data[key] != previous_data[key]:
-                print(f"{key}: {previous_data.get(key, '')} -> {data[key]}")
+    print('\n'.join(changed_files))
 else:
-    print("No changes were detected in the .github/workflows folder since the last merge.")
+    # print("No changes were detected in the .github/workflows folder since the last merge.")
+    print("No changes were detected in the .github/workflows folder.")
+
