723c150e1d825051802dc38617e0577633650986
ValeryKhirin <valeryk@bynet.co.il>
2023-03-05 22:31:26 +0200

    Versioning debug - 2

--- test_version_type.sh
+++ test_version_type.sh
@@ -9,8 +9,21 @@ test() {
     esac
 }
 
-test "feature22"
-test "release/a"
-test "patch.bb"
-test "release-valery"
-test "boom"
+test2(){
+    IFS='.' read -ra version <<< "$(echo 0.0.0)"
+    case "$1" in
+        patch) version[2]=$((version[2]+1)) ;;
+        minor) version[1]=$((version[1]+1)); version[2]=0 ;;
+        major) version[0]=$((version[0]+1)); version[1]=0; version[2]=0 ;;
+    esac
+    VERSION="${version[0]}.${version[1]}.${version[2]}"
+    echo "$VERSION"
+}
+
+# test "feature22"
+# test "release/a"
+# test "patch.bb"
+# test "release-valery"
+# test "boom"
+
+test2 "minor"
